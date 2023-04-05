ba983a000965264fe232adb55c12b1fcbe21ceb0
DoretskiyBV <63367508+Lazard-Live@users.noreply.github.com>
2023-04-05 12:45:50 +0300

    initial commit

---
+++ src/components/MeClock.vue
@@ -0,0 +1,19 @@
+<script setup>
+window.onload = function(){
+  window.setInterval(function(){
+    let now = new Date();
+    let clock = document.getElementById("clock");
+    clock.innerHTML = now.toLocaleTimeString();
+  }, 1000);
+};
+</script>
+
+<template>
+<div id="clock"></div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
